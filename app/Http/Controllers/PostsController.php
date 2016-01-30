53e70f5f53193744a995ca18db8dcbc0c05fe7c2
40143146 <40143146@gm.nfu.edu.tw>
2016-01-30 18:03:58 +0800

    20160130 18:03  Before   {!!Form.......!!}

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Post;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        $data = ['posts' => $posts,];
+        return view('posts.index',$data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+        $posts = Post::all();
+        $data = ['posts' => $posts,];
+
+        return view('posts.create',$data);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::find($id);
+
+        $data = compact('post');
+
+        return view('posts.show', $data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function hot()
+    {
+        $posts = Post::where('page_view','>',100)->orderBy('updated_at','desc')->get();
+
+        $data = compact('posts');
+
+        return view('posts.index',$data);
+    }
+
+    public function random()
+    {
+        $post = Post::all()->random();
+        $data = compact('post');
+        return view('posts.show', $data);
+    }
+}
